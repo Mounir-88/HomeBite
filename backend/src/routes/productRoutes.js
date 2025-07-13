1d7fc0cc78a582cadf4a7f8ecaeff9a86cf1d916
Mounir <munierhaffar@gmail.com>
2025-07-13 22:13:41 +0300

    Backend Folder

---
+++ backend/src/routes/productRoutes.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  const products = await prisma.product.findMany();
+  res.json(products);
+});
+
+export default router;
---
+++ backend/src/server.js
@@ -0,0 +1,8 @@
+import app from './app.js';
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
