a4a25c94ac0e26032e959fb1e8db2e1602f3f023
Mounir <munierhaffar@gmail.com>
2025-07-15 22:02:36 +0300

    bachend, postegre, userTable setup

--- backend/src/server.js
+++
@@ -1,8 +0,0 @@
-import app from './app.js';
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
